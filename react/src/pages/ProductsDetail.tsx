0ae545f321f0d58dbbd360d3f63db0fda02104c0
Quan <ngongquan@gmail.com>
2022-03-30 23:20:22 +0700

    update project redux

--- react/src/pages/ProductsDetail.tsx
+++
@@ -1,7 +0,0 @@
-import react from "react";
-
-const ProductDetail =(props) =>{
-    return(<h1>Deatil</h1>)
-}
-
-export default ProductDetail;
--- react/src/pages/homepage.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react/cjs/react.production.min";
-import { getAll } from "../api/products";
-
-const HomePage =  ()=>{
-    
-    return (<h1>HomePage</h1>)
-}
-export default HomePage;
